2c17c3473fbe8102994fc9492b45c8fe88495e71
ymauri <ymauri@outlook.com>
2020-05-07 20:59:39 +0200

    Use template alert message.

--- views/js/front.js
+++ views/js/front.js
@@ -1,5 +1,7 @@
 let CubacelFront = function() {
     let idProduct;
+    let alertBox = $('.alert_notice').first();
+    let alertDefaultHTML = alertBox.html();
 
     let validatePhone = function(number) {
         var regex = /^(52|53|54|55|57|56|58|59)([0-9]{6})$/;
@@ -24,16 +26,16 @@ let CubacelFront = function() {
                 $('#product_customization_id-' + idProduct).val(response.id_customization);
                 $('.add-' + idProduct).trigger('click');
             } else {
-                alert(response.msg);
+                showAlertMessage(response.msg, 'danger');
             }
         });
         return false;
     }
 
     let addToCart = function() {
-        $('.add-custom').click(function(e) {
+        $('a.add-custom').click(function(e) {
             e.preventDefault();
-            idProduct = $(this).attr('data-id');
+            idProduct = $(this).data('id');
             let phone = $('#phone-' + idProduct);
             let email = $('#email-' + idProduct);
             if (phone.length > 0 && phone.val() && validatePhone(phone.val())) {
@@ -41,9 +43,28 @@ let CubacelFront = function() {
             }
             if (email.length > 0 && email.val() && validateEmail(email.val())) {
                 saveCustomizedData(email);
+            } else {
+                let message = phone.length ? "<i class='fa fa-phone'></i>Revise que el número de móvil tenga <b>8 dígitos</b>" :
+                    email.length ? "<i class='fa fa-envelope'></i>La cuenta de internet debe tener terminar en <b>@nauta.com.cu</b>" :
+                    "<i class='fa fa-info'></i> Formato no válido";
+                showAlertMessage(message, 'danger');
             }
-            return;
+
         });
+    }
+
+    /**
+     * 
+     * @param string message 
+     * @param string type {'info', 'danger', 'warning'}
+     */
+    let showAlertMessage = function(message, type) {
+        let content = `<p class="alert_no_item alert alert-${type}">${message}</p>`;
+        alertBox.html(content).addClass('active');
+
+        setTimeout(function() {
+            alertBox.removeClass('active').html(alertDefaultHTML);
+        }, 5000)
     }
 
     return {
